eca2d4dea33c6398ed03d8941542efc5918614c6
MelvinMelonGit <melvintallme@gmail.com>
2023-04-03 21:38:31 +0800

    Centralised elements to centre of page

--- index.js
+++ index.js
@@ -3,14 +3,34 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!',
-        progress: 40
+        message: '',
+        progress: 90
+      }
+    },
+    methods: {
+      handleLeft() {
+        this.message = ''
+        this.progress--
+        if (this.progress < 0) {
+          this.progress = 0
+          this.message = `You can't go below 0!`
+        }
+      },
+      handleRight() {
+        this.message = ''
+        this.progress++
+        if (this.progress > 100) {
+          this.progress = 100
+          this.message = `You can't go above 100!`
+        }
       }
     },
     computed: {
       cssVars() {
         return {
-          '--value': this.progress
+          '--value': this.progress,
+          '--size': '20rem',
+          '--thickness': '7px'
         }
       }
     }
