4b0aad7890d54910bd095dc31d0bca6a6585fca8
MelvinMelonGit <melvintallme@gmail.com>
2023-04-03 20:57:26 +0800

    Added dynamic functionality for radial progress

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        progress: 40
+      }
+    },
+    computed: {
+      cssVars() {
+        return {
+          '--value': this.progress
+        }
       }
     }
   }).mount('#app')
